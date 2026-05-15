a63859972e530eccc5af4efba24c9093ea580ef2
Tymur Kolovorotnyi <misikovich@Tymur-sin-MacBook-Air.local>
2026-05-16 01:47:57 +0200

    added sha1 generation

--- zip.sh
+++ zip.sh
@@ -2,4 +2,7 @@
 cd "$(dirname "$0")"
 rm -f resource-pack.zip
 zip -r resource-pack.zip pack.mcmeta pack.png assets/ -x "**/.DS_Store"
+sha1=$(shasum -a 1 resource-pack.zip | awk '{print $1}')
+echo "$sha1" > sha1
 echo "Done: resource-pack.zip"
+echo "SHA1: $sha1"
